b4b4d7bac35695bebd37c907402d29851945d177
Diego25ch <102130786+Diego25ch@users.noreply.github.com>
2022-11-23 19:58:09 -0600

    Semana 10

--- src/test/java/com/tienda/TiendaApplicationTests.java
+++ src/test/java/com/tienda/TiendaApplicationTests.java
@@ -1,13 +1,14 @@
 package com.tienda;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TiendaApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+            SpringApplication.run(TiendaApplication.class, args);
 	}
 
 }
